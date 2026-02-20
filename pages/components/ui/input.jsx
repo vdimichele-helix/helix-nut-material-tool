cae222391f706cf9ec62caf9ab8ba48b601bbc76
vdimichele-helix <vdimichele@helixlinear.com>
2026-02-20 10:36:42 -0500

    Delete pages/components directory

--- pages/components/ui/input.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function Input({ className = "", ...props }) {
-  const base =
-    "w-full rounded-xl border border-slate-200 bg-white px-4 py-2 text-sm outline-none focus:ring-2 focus:ring-slate-200";
-  return <input className={`${base} ${className}`} {...props} />;
-}

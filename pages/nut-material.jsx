bb8aeb9fd9c2ec0d9ba0215758dabb84d86ca8cd
vdimichele-helix <vdimichele@helixlinear.com>
2026-02-20 11:29:19 -0500

    Update nut-material.jsx

--- pages/nut-material.jsx
+++ pages/nut-material.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { base44 } from '@/api/base44Client';
 import { Search, Filter, FileDown } from 'lucide-react';
 import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";

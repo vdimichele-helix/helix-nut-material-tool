bb0de6802b7db27edc05b4b36e6ae05a4c822e6b
vdimichele-helix <vdimichele@helixlinear.com>
2026-02-20 09:16:18 -0500

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useState } from 'react'
+
+export default function App({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
+}
